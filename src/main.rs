0c800a8b6c5d98353a1958b1ef7274a6610a3b0e
Хаустов Александр Дмитриевич <khaustovad@trcont.ru>
2024-03-25 19:41:00 +0300

    Add thermometr

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,17 @@ struct Outlet {
     state: OutletState,
 }
 
+struct Thermometer {
+    temperature: i32,
+}
+
 trait OutletInterface {
     fn turn_switch(&mut self, state: &bool);
     fn get_power(&self);
+}
+
+trait ThermometerInterface {
+    fn get_temparature(&self);
 }
 
 impl OutletInterface for Outlet {
@@ -30,11 +38,20 @@ impl OutletInterface for Outlet {
     }
 }
 
+impl ThermometerInterface for Thermometer {
+    fn get_temparature(&self) {
+        let current_temperature = self.temperature;
+        println!("Current temperature: {current_temperature:?}");
+    }
+}
+
 fn main() {
     let mut outlet = Outlet {
         power: 3.6,
         state: OutletState::IsOff,
     };
+    let temp = Thermometer { temperature: 23 };
     outlet.get_power();
     outlet.turn_switch(&true);
+    temp.get_temparature();
 }
