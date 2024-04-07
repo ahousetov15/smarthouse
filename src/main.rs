9bda63b43a6a74b613211f095dba58b335d78209
Хаустов Александр Дмитриевич <khaustovad@trcont.ru>
2024-04-07 09:59:17 +0300

    +1

--- src/main.rs
+++ src/main.rs
@@ -13,35 +13,62 @@ struct Socket {
 
 struct Thermometer {
     temperature: i32, // Температура в градусах цельсия
+
+}
+
+/// Для хранения разных типов устройств в одном контейнере,
+/// создаем некий тип-перечисление типов.
+enum Device {
+    Socket(Socket),
+    Thermometer(Thermometer),
 }
 
 ///Наш умный дом
 struct Smarthouse {
-    outlet: Socket,           //Розетка
+    name: String,             // Имя дома
+    socket: Socket,           //Розетка
     thermometer: Thermometer, //Термометр
 }
 
-trait SocketInterface {
-    fn turn_switch(&mut self, state: &bool); //Повернуть выключатель (во Вкл. или Выкл.)
-    fn get_power(&self); // Получить потребляемую мощность
+/// Команата
+struct Room {
+    name: String,
+    decives: Vec<Device>,
 }
+
+// trait SocketInterface {
+//    fn turn_switch(&mut self, state: &bool); //Повернуть выключатель (во Вкл. или Выкл.)
+//    fn get_power(&self); // Получить потребляемую мощность
+// }
 
 trait ThermometerInterface {
     fn get_temparature(&self); //Получить текущую температуру
 }
 
 ///Интерфейс для умного дома
+// trait SmarthouseInterface {
+//    fn new() -> Self; //Конструктор для инициализации со значениями по умолчанию
+//    fn temperature(&self); //Получить текущую температуру
+//    fn turn_switch(&mut self, turn: &bool); //Повернуть выключатель
+//    fn socket_power(&self); //Получить потребляемую мощность
+// }
 trait SmarthouseInterface {
-    fn new() -> Self; //Конструктор для инициализации со значениями по умолчанию
-    fn temperature(&self); //Получить текущую температуру
-    fn turn_switch(&mut self, turn: &bool); //Повернуть выключатель
-    fn outlet_power(&self); //Получить потребляемую мощность
+    fn new() where Self: Sized; //Конструктор для инициализации со значениями по умолчанию
+    fn get(&self); //Получить текущую температуру
+    fn interact(&mut self, turn: &bool); //Повернуть выключатель
 }
 
 ///Интерфейс для розетки
-impl SocketInterface for Socket {
+impl SmarthouseInterface for Socket {
+    fn new() {
+        Socket{
+            power: 0.0,
+            state: SocketState::IsOff,
+        }
+    }
+
     /// Повернуть выключатель (во Вкл. или Выкл.)
-    fn turn_switch(&mut self, state: &bool) {
+    fn interact(&mut self, state: &bool) {
         if *state {
             self.state = SocketState::IsOn;
             println!("Socket is switched on");
@@ -52,54 +79,64 @@ impl SocketInterface for Socket {
     }
 
     /// Получить потребляемую мощность
-    fn get_power(&self) {
+    fn get(&self) {
         let value = self.power;
         println!("Socket power is {value} vatt");
     }
 }
 
 ///Интерфейс для термометра
-impl ThermometerInterface for Thermometer {
+impl SmarthouseInterface for Thermometer {
+    fn new() -> () {
+        Thermometer{
+            temperature: 23,
+        }
+    }
+
     ///Получить текущую температуру
-    fn get_temparature(&self) {
+    fn get(&self) {
         let current_temperature = self.temperature;
         println!("Current temperature: {current_temperature:?}");
+    }
+
+    fn interact(&mut self, turn: &bool) {
+        println!("Turned {}", if *turn { "on" } else { "off" });
     }
 }
 
 ///! Имплементация интерфейса для умного дома
-impl SmarthouseInterface for Smarthouse {
-    /// Конструктор для инициализации со значениями по умолчанию
-    fn new() -> Self {
-        Self {
-            outlet: Socket {
-                power: 3.6,
-                state: SocketState::IsOff,
-            },
-            thermometer: Thermometer { temperature: 23 },
-        }
-    }
+// impl SmarthouseInterface for Smarthouse {
+//     /// Конструктор для инициализации со значениями по умолчанию
+//     fn new() -> Self {
+//         Self {
+//             socket: Socket {
+//                 power: 3.6,
+//                 state: SocketState::IsOff,
+//             },
+//             thermometer: Thermometer { temperature: 23 },
+//         }
+//     }
 
-    /// Получить текущую температуру
-    fn temperature(&self) {
-        self.thermometer.get_temparature();
-    }
+//     /// Получить текущую температуру
+//     fn temperature(&self) {
+//         self.thermometer.get_temparature();
+//     }
 
-    /// Повернуть выключатель
-    fn turn_switch(&mut self, turn: &bool) {
-        self.outlet.turn_switch(turn);
-    }
+//     /// Повернуть выключатель
+//     fn turn_switch(&mut self, turn: &bool) {
+//         self.socket.turn_switch(turn);
+//     }
 
-    /// Получить потребляемую мощность
-    fn outlet_power(&self) {
-        self.outlet.get_power();
-    }
-}
+//     /// Получить потребляемую мощность
+//     fn socket_power(&self) {
+//         self.socket.get_power();
+//     }
+// }
 
 fn main() {
-    let mut smarthouse = Smarthouse::new(); //Создание экземпляра умного дома
-    smarthouse.outlet_power(); //Получить потребляемую мощность
-    smarthouse.turn_switch(&true); //Повернуть выключатель
-    smarthouse.turn_switch(&false); //Повернуть выключатель
-    smarthouse.temperature(); //Получить текущую температуру
+    // let mut smarthouse = Smarthouse::new(); //Создание экземпляра умного дома
+    // smarthouse.socket_power(); //Получить потребляемую мощность
+    // smarthouse.turn_switch(&true); //Повернуть выключатель
+    // smarthouse.turn_switch(&false); //Повернуть выключатель
+    // smarthouse.temperature(); //Получить текущую температуру
 }
